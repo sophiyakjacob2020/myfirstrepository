e97d9e170458bd6f013a351d46e555602b019210
sophiyakjacob2020 <111106657+sophiyakjacob2020@users.noreply.github.com>
2022-08-12 10:36:26 +0530

    updated HelloWorld.java
    Added Hello World through system.out.println as well.

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class String {
 	public static void main(java.lang.String[] args) {
 		java.lang.String name = "Hello World";
 		System.out.println(name);
+		System.out.println("Hello World");
 
 	}
 
